b69b47d97a33bed9cbd825213720a196dabc1581
peppe2412 <peppeterrazzano79@gmail.com>
2025-09-06 21:33:55 +0200

    aggiunta descrizione alle cards

--- rolls/models.py
+++ rolls/models.py
@@ -5,6 +5,7 @@ class Roll(models.Model):
     img = models.ImageField()
     name = models.CharField(max_length=30)
     price = models.FloatField()
+    description = models.CharField(default='no descrizione')
     
     def __str__(self):
         return self.name
